53cfcd7e0af7978f63ba1ab9f88b69cd2d1816da
Marco A. Schneiders <marcoschneiders@live.com>
2025-02-03 13:58:15 -0300

    Button Exit/Enter

--- lib/main.dart
+++ lib/main.dart
@@ -18,27 +18,65 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void decrement() {
+    print('decrement');
+  }
+
+  void increment() {
+    print('increment');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
           Text('Texto',
-          style: TextStyle(
-              fontFamily: 'serif',
-              fontSize: 70,
-              color: Colors.white,
-              fontWeight: FontWeight.w800,)),
-      Text('0',
-          style: TextStyle(
-              fontFamily: 'serif',
-              fontSize: 90,
-              color: Colors.white,
-              fontWeight: FontWeight.w800,))],
-    )
-    ,
+              style: TextStyle(
+                fontFamily: 'serif',
+                fontSize: 70,
+                color: Colors.black,
+                fontWeight: FontWeight.w800,
+              )),
+
+          Text('0',
+              style: TextStyle(
+                fontFamily: 'serif',
+                fontSize: 90,
+                color: Colors.black,
+                fontWeight: FontWeight.w800,
+              )),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: decrement,
+                child: Text('Saiu',
+                    style: TextStyle(
+                      fontFamily: 'serif',
+                      fontSize: 20,
+                      color: Colors.black,
+                    )),
+              ),
+
+              TextButton(
+                onPressed: increment,
+                child: Text('Entrou',
+                    style: TextStyle(
+                      fontFamily: 'serif',
+                      fontSize: 20,
+                      color: Colors.black,
+                    )),
+              ),
+
+            ],
+          )
+        ],
+      ),
     );
   }
 }
